0913936fb557d6fb32816fe70fe65c66e07fa328
Juan Vitali <juan.vitali90@gmail.com>
2020-06-09 19:52:36 -0300

    PrimerCommit

--- src/obligatorio/Sistema.java
+++ src/obligatorio/Sistema.java
@@ -17,130 +17,157 @@ public class Sistema implements ISistema {
         this.maxCantPalabras = maxCantPalabras;
         this.carpetas = new ListaCarpetas();
     }
-    
-    
 
     @Override
     public Retorno CrearSistemaMensajes() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.OK);
+        int[][] ciudades = {{0, 10, 25, 15, 30, 0}, {10, 0, 20, 0, 0, 0}, {25, 20, 0, 0, 0, 40}, {15, 0, 0, 0, 0, 45}, {30, 0, 0, 0, 0, 25}, {0, 0, 40, 45, 25, 0}};
+        carpetas = new ListaCarpetas();
+        diccionario = new ListaPalabras();
+        return r;
     }
 
     @Override
     public Retorno DestruirSistemaMensajes() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
 
     @Override
-    public Retorno AgregarCarpeta(String unidad, String carpeta) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Retorno AgregarCarpeta(String unidad, String carpeta) { //Que hacer con la unidad
+        Retorno r = new Retorno(Retorno.Resultado.OK);
+        carpetas.Agregar(carpeta);
+        return r;
     }
 
     @Override
     public Retorno EliminarCarpeta(String unidad, String carpeta) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        
+        return r;
     }
 
     @Override
     public Retorno ListarEstructura(String unidad, String carpeta) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
-    
+
     @Override
     public Retorno AgregarMensaje(String unidad, String carpeta, String mensaje) {
         NodoArchivo nuevo = new NodoArchivo(mensaje);
         //en lugar de ser get inicio deberia ser buscar por unidad y nombre carpeta
         carpetas.getInicio().getArchivos().Agregar(nuevo);
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
 
     @Override
     public Retorno EliminarMensaje(String unidad, String carpeta, String mensaje) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
 
     @Override
     public Retorno InsertarLinea() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
 
     @Override
     public Retorno InsertarLineaEnPosicion(int posicionLinea) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
 
     @Override
     public Retorno BorrarLinea(int posicionLinea) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
 
     @Override
     public Retorno BorrarTodo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
 
     @Override
     public Retorno BorrarOcurrenciasPalabraEnTexto(String palabraABorrar) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
 
     @Override
     public Retorno ImprimirTexto() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
 
     @Override
     public Retorno InsertarPalabraEnLinea(int posicionLinea, int posicionPalabra, String palabraAIngresar) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
 
     @Override
     public Retorno InsertarPalabraYDesplazar(int posicionLinea, int posicionPalabra, String palabraAIngresar) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
 
     @Override
     public Retorno BorrarPalabra(int posicionLinea, int posicionPalabra) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
 
     @Override
     public Retorno BorrarOcurrenciasPalabraEnLinea(int posicionLinea, String palabraABorrar) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
 
     @Override
     public Retorno ImprimirLinea(int posicionLinea) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retornfo.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
 
     @Override
     public Retorno IngresarPalabraDiccionario(String palabraAIngresar) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
 
     @Override
     public Retorno BorrarPalabraDiccionario(String palabraABorrar) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
 
     @Override
     public Retorno ImprimirDiccionario() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
 
     @Override
     public Retorno ImprimirTextoIncorrecto() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
 
     @Override
     public Retorno CargarDistancias(int[][] Ciudades) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
 
     @Override
     public Retorno BuscarCamino(int[][] M, String origen, String destino) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Retorno r = new Retorno(Retorno.Resultado.NO_IMPLEMENTADA);
+        return r;
     }
 
 }
